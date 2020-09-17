ab129ec03dafe5d0773947119b05a05433c70d03
takasehideki <takase@i.kyoto-u.ac.jp>
2020-09-17 14:19:47 +0900

    Upgrade IPとPlatform Designer(QSys)を実行して再合成

--- quartus_project/DE10_NANO_SoC_FB/ip/vga_pll/vga_pll_sim/synopsys/vcs/vcs_setup.sh
+++ quartus_project/DE10_NANO_SoC_FB/ip/vga_pll/vga_pll_sim/synopsys/vcs/vcs_setup.sh
@@ -1,5 +1,5 @@
 
-# (C) 2001-2016 Altera Corporation. All rights reserved.
+# (C) 2001-2020 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 16.0 222 win32 2016.12.05.18:43:25
+# ACDS 18.1 625 win32 2020.09.17.13:43:52
 
 # ----------------------------------------
 # vcs - auto-generated simulation script
@@ -94,14 +94,13 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 16.0 222 win32 2016.12.05.18:43:25
+# ACDS 18.1 625 win32 2020.09.17.13:43:52
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="vga_pll"
 QSYS_SIMDIR="./../../"
-QUARTUS_INSTALL_DIR="D:/altera/16.0/quartus/"
+QUARTUS_INSTALL_DIR="C:/intelfpga_lite/18.1/quartus/"
 SKIP_FILE_COPY=0
-SKIP_ELAB=0
 SKIP_SIM=0
 USER_DEFINED_ELAB_OPTIONS=""
 USER_DEFINED_SIM_OPTIONS="+vcs+finish+100"
